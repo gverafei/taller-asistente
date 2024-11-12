650097b67f3ff487a7cc72faea87910ee35fb70d
Guillermo Humberto Vera Amaro <143739705+gverafei@users.noreply.github.com>
2024-11-11 22:33:40 -0600

    practica8

--- practica8/website.py
+++ practica8/website.py
@@ -3,6 +3,8 @@ from subprocess import call
 
 def open_chrome(website):
     website = "" if website is None else website
-    # Funciona para windows, si tienes otro navegador, busca el ejecutable
+    # Funciona para windows, si tienes otro SO o navegador, busca el ejecutable
     call("C:/Program Files/Google/Chrome/Application/chrome.exe " + website)
+    # Quizas funcione este para linux
+    #call("google-chrome" + website)
     return "Listo, ya abrí el sitio web solicitado."
